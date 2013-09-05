67a519220c8ff5ff912b8722c8341b719d3f13c7
Abu Ashraf Masnun <masnun@gmail.com>
2013-09-06 00:06:14 +0600

    python version added

---
+++ python/main.py
@@ -0,0 +1,10 @@
+import webapp2
+
+class Main(webapp2.RequestHandler):
+  def get(self):
+    self.response.out.write('Hello DevFest 2013!')
+
+    
+app = webapp2.WSGIApplication([
+  ('/', Main),
+], debug=True)
