28dfabb67330c60bb092ff8c3ba3d88c47e58842
Abu Ashraf Masnun <masnun@gmail.com>
2013-09-06 10:04:23 +0600

    added ',' after hello

--- python/main.py
+++ python/main.py
@@ -2,7 +2,7 @@ import webapp2
 
 class Main(webapp2.RequestHandler):
   def get(self):
-    self.response.out.write('Hello DevFest 2013!')
+    self.response.out.write('Hello, DevFest 2013!')
 
     
 app = webapp2.WSGIApplication([
